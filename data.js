92c1fb223ad5dd56fa198894d0555c46f75ac4ea
Nam0 <gianni.blazquez@gmail.com>
2026-01-22 13:13:58 +0100

    Add data array with options and descriptions

---
+++ data.js
@@ -0,0 +1,5 @@
+const data = [
+  { title: "Option 1", description: "Description de l'option 1", value: 10 },
+  { title: "Option 2", description: "Description de l'option 2", value: 20 },
+  { title: "Option 3", description: "Description de l'option 3", value: 30 }
+];
